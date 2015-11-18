795c494a5102c7d700f786fd52e0954cb43a2617
TrubkaU <dubrava123@gmail.com>
2015-11-18 14:19:29 +0300

    Add parcer image

--- Simple.py
+++ Simple.py
@@ -10,28 +10,34 @@ def parse(html):
     soup = BeautifulSoup(html)
     table = soup.find('div',id='content')
 
-    titles = []
+    list = []
 
     for rows in table.find_all('div','post'):
 
-        titles.append({
-
-            'image': rows.find('img').src
+        list.append({
+            'text': rows.p.text,
+            'image': rows.img["src"],
+            'title': rows.h1.a["title"]
         })
 
-    for title in titles: print(title)
+    return list
 
+def print_result(list):
+     for entry in list:
+
+        data = entry['title']
+        titl = entry['text']
+        title = entry['image']
+
+        print(titl)
+        print(title)
+        print(data)
 
 def main():
 
    html = get_html("http://www.graffiti.by/")
-   parse(html)
-
+   list = parse(html)
+   print_result(list)
 
 if __name__ == '__main__':
         main()
-
-
-
-
-
