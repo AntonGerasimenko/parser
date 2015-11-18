23c4810424108e170fdf3723c6e4e60c98e2a787
TrubkaU <dubrava123@gmail.com>
2015-11-18 16:03:32 +0300

    Add DB

--- Simple.py
+++ Simple.py
@@ -1,5 +1,7 @@
 import urllib
 from bs4 import BeautifulSoup
+import _sqlite3
+
 def get_html(url):
 
     response = urllib.urlopen(url)
@@ -17,7 +19,8 @@ def parse(html):
         list.append({
             'text': rows.p.text,
             'image': rows.img["src"],
-            'title': rows.h1.a["title"]
+            'title': rows.h1.a["title"],
+            'id': rows["id"]
         })
 
     return list
@@ -25,19 +28,48 @@ def parse(html):
 def print_result(list):
      for entry in list:
 
-        data = entry['title']
-        titl = entry['text']
-        title = entry['image']
+        title = entry['title']
+        text = entry['text']
+        image = entry['image']
+        id = entry['id']
 
-        print(titl)
         print(title)
-        print(data)
+        print(text)
+        print(image)
+        print(id)
+
+
+
+def db(events_list):
+
+    conn = _sqlite3.connect('events.db')
+    conn.execute('''CREATE TABLE IF NOT EXISTS EVENTS
+       (ID INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+       TITLE           TEXT    NOT NULL,
+       _TEXT            TEXT     NOT NULL,
+       IMAGE        TEXT NOT NULL );''')
+    print "Table created successfully"
+
+    for event in events_list:
+
+        title = "'" + event['title'] +"'"
+        _text = "'" + event['text'] +"'"
+        image = "'" + event['image'] +"'"
+
+        string = "INSERT INTO EVENTS (TITLE,_TEXT,IMAGE) VALUES ("+title+","+_text+","+image+")"
+        conn.execute(string)
+
+    conn.commit()
+    print "Records created successfully"
+    conn.close()
 
 def main():
 
    html = get_html("http://www.graffiti.by/")
-   list = parse(html)
-   print_result(list)
+   events_list = parse(html)
+   #db(events_list)
+
+   print_result(events_list)
 
 if __name__ == '__main__':
         main()
