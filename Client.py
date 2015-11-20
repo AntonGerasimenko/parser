a5fea49238dbb2f2f69e08f8242d9e292e373f17
TrubkaU <dubrava123@gmail.com>
2015-11-20 13:43:03 +0300

    Full response- request

--- Client.py
+++ Client.py
@@ -11,6 +11,7 @@ def get_Json():
 
     data = {}
     data['key'] = 'value'
+    data['pass'] = 'bober'
     json_data = json.dumps(data)
 
     return json_data
@@ -19,17 +20,13 @@ def get_Json():
 def post(url):
 
     json = get_Json()
-
     r = requests.post(url,None,json, auth=('user', 'pass'))
 
-    return r.links
-
-
+    return r.json()
 
 def main():
 
     print (post('http://127.0.0.1'))
-
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,33 +1,42 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-
+import Client
+import cgi
 class HttpProcessor(BaseHTTPRequestHandler):
-
 
     def do_POST(self):
 
+        if 'application/json' == self.headers.get('content-type'):
 
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
 
+            print(form)
 
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
 
+            json = Client.get_Json()
+            self.wfile.write(json)
 
+        else :
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
 
-
-
-        self.wfile.write("hello !")
+            self.wfile.write("This HTML response")
 
     def do_GET(self):
 
         self.send_response(200)
-        self.send_header('content-type','text/html')
+        self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        self.wfile.write("hello !")
-        self.wfile.write("ggg")
 
+        self.wfile.write("This HTML response")
 
 serv = HTTPServer(("localhost",80),HttpProcessor)
 serv.serve_forever()
-
-print(serv)
