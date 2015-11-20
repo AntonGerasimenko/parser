18af4ac038915df619d66a2cbdf1b7efe45975f2
TrubkaU <dubrava123@gmail.com>
2015-11-20 16:38:34 +0300

    Create dictionary after did response

--- server.py
+++ server.py
@@ -1,27 +1,24 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import Client
-import cgi
+import json
 class HttpProcessor(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
         if 'application/json' == self.headers.get('content-type'):
 
-            form = cgi.FieldStorage(
-                fp=self.rfile,
-                headers=self.headers,
-                environ={'REQUEST_METHOD':'POST',
-                         'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
+            length = int(self.headers.getheader('content-length'))
+            data = self.rfile.read(length)
+            resp = json.loads(data)
+            dict = json.loads(str(resp))
 
-            print(form)
 
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
 
-            json = Client.get_Json()
-            self.wfile.write(json)
+            jsn = Client.get_Json()
+            self.wfile.write(jsn)
 
         else :
             self.send_response(200)
@@ -32,11 +29,11 @@ class HttpProcessor(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
-        self.end_headers()
+         self.send_response(200)
+         self.send_header('Content-Type', 'text/html')
+         self.end_headers()
 
-        self.wfile.write("This HTML response")
+         self.wfile.write("This HTML response")
 
 serv = HTTPServer(("localhost",80),HttpProcessor)
 serv.serve_forever()
